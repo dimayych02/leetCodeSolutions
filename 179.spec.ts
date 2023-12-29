0fb6bb3890ee04641728ad16a15529a78cc53f5c
Дмитрий Кирилин <dim_dimyych@mail.ru>
2023-12-30 02:47:21 +0400

    179 issue

---
+++ 179.spec.ts
@@ -0,0 +1,13 @@
+179. Largest Number
+
+
+function largestNumber(nums: number[]): string {
+    const compare = (a: number, b: number): number => {
+        const num1: string = `${a}${b}`;
+        const num2: string = `${b}${a}`;
+        return num1 < num2 ? 1 : -1;
+    }
+    const rs: number[] = nums.sort(compare);
+    if(rs[0]===0) return '0';
+    return rs.sort(compare).join('');
+};
