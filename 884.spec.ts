1cb7de70a50d813d850ebf537e87ce8e02e129ed
Дмитрий Кирилин <106656570+dimayych02@users.noreply.github.com>
2023-12-31 00:47:33 +0400

    Update 884.spec.ts
    some changes

--- 884.spec.ts
+++ 884.spec.ts
@@ -5,7 +5,8 @@ const uncommonFromSentences = (s1: string, s2: string): string[] => {
     const strArrOne: string[] = s1.split(' ');
     const strArrTwo: string[] = s2.split(' ');
     const unCommonOne = strArrOne.filter(
-        (x, index) => !strArrTwo.includes(x) && strArrOne.indexOf(x) === strArrOne.lastIndexOf(x));
+        (x, index) => !strArrTwo.includes(x) && strArrOne.indexOf(x) === strArrOne.lastIndexOf(x)
+    );
     const unCommonTwo = strArrTwo.filter(
         (y, index) => !strArrOne.includes(y) && strArrTwo.indexOf(y) === strArrTwo.lastIndexOf(y)
     );
