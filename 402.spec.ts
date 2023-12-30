ac906834a9a8903430b3aa208ad65929e204cd3b
Дмитрий Кирилин <dim_dimyych@mail.ru>
2023-12-31 00:46:11 +0400

    402 issue

---
+++ 402.spec.ts
@@ -0,0 +1,21 @@
+402. Remove K Digits
+
+
+/** @param num - non-negative integer */
+/** @param k - removing k-digits */
+function removeKdigits(num: string, k: number): string {
+    const stack: string[] = [];
+    for (const digit of num) {
+        while (k > 0 && stack.length > 0 && stack[stack.length - 1] > digit) {
+            stack.pop();
+            k--;
+        }
+        stack.push(digit);
+    }
+    while (k > 0) {
+        stack.pop();
+        k--;
+    }
+    const result = stack.join('').replace(/^0+/, '');
+    return result.length > 0 ? result : '0';
+}
