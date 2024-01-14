49dcf5d587e2982daf0964389dcb36d0dc02d76a
Дмитрий Кирилин <106656570+dimayych02@users.noreply.github.com>
2024-01-14 20:58:55 +0400

    Update 1876.spec.ts
    certain changes

--- 1876.spec.ts
+++ 1876.spec.ts
@@ -4,7 +4,7 @@
 function countGoodSubstrings(s: string): number {
     const set = new Set<string>();
     let counter: number = 0;
-    while (s.length > 0) {
+    while (s.length >= 3) {
         let leftBorder: number = 0;
         const rightBorder: number = 3;
 
@@ -15,8 +15,6 @@ function countGoodSubstrings(s: string): number {
         counter = (set.size === 3) ? counter + 1 : counter;
         set.clear();
         s = s.substring(1);
-        if (s.length < 3)
-            break;
     }
     return counter;
 
