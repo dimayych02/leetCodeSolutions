4396e47d5950ae1e50ad5dd23c11faa5db276878
Дмитрий Кирилин <106656570+dimayych02@users.noreply.github.com>
2024-02-03 03:21:11 +0400

    Update 56.spec.ts

--- 56.spec.ts
+++ 56.spec.ts
@@ -6,12 +6,16 @@ function merge(arr: number[][]): number[][] {
     arr.sort((a, b) => a[0] - b[0]);
     const rs: number[][] = [arr[0]];
     for (const interval of arr) {
-        let prev: number[] = rs[rs.length - 1];
+        const last: number = rs.length - 1;
+        const prev: number[] = rs[last];
+
         if (prev[1] >= interval[0])
-            prev[1] = Math.max(prev[1], interval[1]);
-            
+            // Update length
+            rs[last][1] = Math.max(prev[1], interval[1]);
+
         else
-            rs.push(interval);    
+            rs.push(interval);
     }
+
     return rs;
 }
