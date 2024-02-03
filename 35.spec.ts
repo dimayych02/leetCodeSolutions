45104c2bf76b9729fec19c70e9972d034a8b90ba
Дмитрий Кирилин <106656570+dimayych02@users.noreply.github.com>
2024-02-03 20:01:55 +0400

    Create 35.spec.ts

---
+++ 35.spec.ts
@@ -0,0 +1,24 @@
+const searchInsert = (nums: number[], target: number): number => {
+    let startIndex: number = 0;
+    let endIndex: number = nums.length - 1;
+    let middleIndex: number = 0;
+    while (startIndex <= endIndex) {
+        middleIndex = startIndex + Math.floor((endIndex - startIndex) / 2);
+
+        if (nums[middleIndex] === target)
+            return middleIndex;
+
+        if (nums[middleIndex] > target)
+            endIndex = middleIndex - 1;
+        else
+            startIndex = middleIndex + 1;
+    }
+
+    const rs: number =
+        (target > nums[middleIndex - 1] && target < nums[middleIndex])
+            ? middleIndex :
+            (target < nums[middleIndex])
+                ? Math.max(0, middleIndex - 1)
+                : middleIndex + 1
+    return rs;
+};
