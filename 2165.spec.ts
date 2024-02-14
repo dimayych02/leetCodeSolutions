d099fa7708ad1b90ff15b2bc6e4695342eac98f5
Дмитрий Кирилин <106656570+dimayych02@users.noreply.github.com>
2024-02-14 22:52:57 +0400

    2165 issue

---
+++ 2165.spec.ts
@@ -0,0 +1,44 @@
+/*
+You are given an integer num. Rearrange the digits of num such that its value is minimized and it does not contain any leading zeros.
+
+Return the rearranged number with minimal value.
+
+Note that the sign of the number does not change after rearranging the digits.
+*/
+
+function smallestNumber(num: number): number {
+
+    const hasZero: boolean = String(num)[0] == '-';
+
+    let sortArr: number[] = String(num)
+        .replace('-', '')
+        .split('')
+        .map(Number)
+        .sort((a, b) => hasZero ? b - a : a - b);
+    findAccEl(sortArr);
+  
+    const rs =  hasZero
+        ? '-'.concat(sortArr.join(''))
+        : sortArr.join('')
+    return Number(rs);
+}
+
+const findAccEl = (arr: number[]): void => {
+    let l: number = 0;
+    let r: number = arr.length - 1;
+    if (arr[0] !== 0)
+        return;
+
+    while (l < r) {
+        const mid = l + Math.floor((r - l) / 2);
+
+        if (arr[mid] > 0)
+            r = mid;
+        else
+            l = mid + 1;
+    }
+
+    const temp = arr[r];
+    arr[0] = temp;
+    arr[r] = 0;
+}
