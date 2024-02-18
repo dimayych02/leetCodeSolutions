e26a2f7ee8c0abf887e78f53e73952c201a2cafd
Дмитрий Кирилин <106656570+dimayych02@users.noreply.github.com>
2024-02-19 00:42:48 +0400

    Update 739.spec.ts

--- 739.spec.ts
+++ 739.spec.ts
@@ -4,7 +4,6 @@ return an array answer such that answer[i] is the number of days you have to wai
 If there is no future day for which this is possible, keep answer[i] == 0 instead.
 */
 
-
 function dailyTemperatures(temperatures: number[]): number[] {
 
     const res: number[] = new Array(temperatures.length).fill(0);
@@ -12,7 +11,7 @@ function dailyTemperatures(temperatures: number[]): number[] {
 
     for (let i = 0; i < temperatures.length; i++) {
         const t = temperatures[i];
-        while (stack.length > 0 && t > stack[stack.length - 1][1]) {
+        while (stack.length && t > stack[stack.length - 1][1]) {
             const stackIdx: number = stack[stack.length - 1][0];
             res[stackIdx] = i - stackIdx
             stack.pop();
