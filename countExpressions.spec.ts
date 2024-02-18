188066f759677241f32378bae173c15326caf268
Дмитрий Кирилин <106656570+dimayych02@users.noreply.github.com>
2024-02-18 16:38:04 +0400

    count mathematics expression

---
+++ countExpressions.spec.ts
@@ -0,0 +1,84 @@
+// You are given an expressions like '6+3*(1+4*5)*2'
+// Task is - to count it
+// The result will be 132
+
+function countPolishSystem(expr: string): string {
+    expr = infixToPostfix(expr);
+    let res: string = '';
+    const stack: string[] = [];
+
+    for (const char of expr) {
+
+        const regExp = new RegExp(/[0-9]/);
+        if (regExp.test(char))
+            stack.push(char);
+
+        else {
+
+            const second = stack.pop();
+            const first = stack.pop();
+
+            switch (char) {
+
+                case '+': res = `${Number(first) + Number(second)}`
+                    break;
+                case '-': res = `${Number(first) - Number(second)}`
+                    break;
+
+                case '*': res = `${Number(first) * Number(second)}`
+                    break;
+
+                case '/': res = `${Number(first) / Number(second)}`
+                    break;
+
+            }
+
+            stack.push(res);
+        }
+    }
+
+    return res;
+}
+
+// Convert our expression (ex '6+3*(1+4*5)*2' ) to Polish system
+function infixToPostfix(expression: string): string {
+
+    // Table with priorities
+    const hashTable: Record<string, number> =
+    {
+        '+': 1,
+        '-': 1,
+        '*': 2,
+        '/': 2,
+    };
+
+    let res: string = '';
+    const stack: string[] = [];
+
+    for (const char of expression) {
+
+        if (char.match(/[0-9]/))
+            res += char;
+
+        else if (char === '(')
+            stack.push(char);
+
+        else if (char === ')') {
+            while (stack.length && stack[stack.length - 1] !== '(')
+                res += stack.pop();
+            stack.pop();
+        }
+
+        else {
+            while (stack.length && hashTable[stack[stack.length - 1]] >= hashTable[char])
+                res += stack.pop();
+            stack.push(char);
+        }
+    }
+
+    // Push items in case if stack not empty
+    while (stack.length)
+        res += stack.pop();
+
+    return res;
+}
