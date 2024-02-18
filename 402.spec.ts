7bb529332d046a0cb5d75cd883fa85c9938b3137
Дмитрий Кирилин <106656570+dimayych02@users.noreply.github.com>
2024-02-18 19:40:19 +0400

    Update 402.spec.ts

--- 402.spec.ts
+++ 402.spec.ts
@@ -4,18 +4,23 @@
 /** @param num - non-negative integer */
 /** @param k - removing k-digits */
 function removeKdigits(num: string, k: number): string {
+
     const stack: string[] = [];
+
     for (const digit of num) {
         while (k > 0 && stack.length > 0 && stack[stack.length - 1] > digit) {
             stack.pop();
             k--;
         }
         stack.push(digit);
+
     }
+
     while (k > 0) {
         stack.pop();
         k--;
     }
-    const result = stack.join('').replace(/^0+/, '');
-    return result.length > 0 ? result : '0';
-}
+
+    const res = stack.join('').replace(/^0+/, '');
+    return res.length > 0 ? res : '0';
+};
