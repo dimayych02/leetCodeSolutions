a0d9956a3fee14af605b793ae26e55ee2f835bf8
Дмитрий Кирилин <106656570+dimayych02@users.noreply.github.com>
2024-02-24 00:13:32 +0400

    Update 1209.spec.ts

--- 1209.spec.ts
+++ 1209.spec.ts
@@ -9,24 +9,24 @@ Return the final string after all such duplicate removals have been made. It is 
 */
 
 function removeDuplicates(s: string, k: number): string {
-  
-  if(k > s.length)
-      return s;
-    
-  let res: string = '';
-  const stack: [string,number][] = [];  
-  for(const letter of s) {
-      if(stack.length && stack[stack.length-1][0] == letter)
-          stack[stack.length-1][1]+=1;    
-      else
-          stack.push([letter, 1]);
-      
-      if(stack.length && stack[stack.length-1][1]===k)
-          stack.pop();
-  }
-    
-  for(const pairs of stack) {
-      res+= pairs[0].repeat(pairs[1]);
-  }  
-return res
+
+    if (k > s.length)
+        return s;
+
+    let res: string = '';
+    const stack: [string, number][] = [];
+    for (const letter of s) {
+        if (stack.length && stack[stack.length - 1][0] == letter)
+            stack[stack.length - 1][1] += 1;
+        else
+            stack.push([letter, 1]);
+
+        if (stack.length && stack[stack.length - 1][1] === k)
+            stack.pop();
+    }
+
+    for (const pair of stack) {
+        res += pair[0].repeat(pair[1]);
+    }
+    return res
 }
