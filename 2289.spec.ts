a482759fa7e917f1bdcd36c9bfc7a02dd3e8e771
Дмитрий Кирилин <106656570+dimayych02@users.noreply.github.com>
2024-03-02 21:59:25 +0400

    Update 2289.spec.ts

--- 2289.spec.ts
+++ 2289.spec.ts
@@ -7,19 +7,19 @@ Return the number of steps performed until nums becomes a non-decreasing array.
 /** @param nums - array of numbers */
 const totalSteps = (nums: number[]): number => {
 
-    let ans: number = 0;
     const stack: number[][] = [];
-    
-    for (const val of nums) {
-        let stp: number = 0;
-        
-        while (stack.length && stack[stack.length - 1][1] > val) {
-            stp = Math.max(stp + 1, stack.pop()[0]);
-        }
 
-        stack.push([stp, val]);
+    for (let idx = nums.length - 1; idx >= 0; idx--) {
+        const value: number = nums[idx];
+        let stp: number = 0;
+
+        while (stack.length && stack[stack.length - 1][1] < value) 
+            stp = Math.max(stp + 1, stack.pop()[0]);
+        
+        stack.push([stp, value]);
     }
 
+    let ans: number = 0;
     while (stack.length)
         ans = Math.max(ans, stack.pop()[0]);
 
