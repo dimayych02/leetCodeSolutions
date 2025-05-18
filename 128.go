3cc4e15cf795d4259ca8036d3c5f5912e2aceb60
Dmitrij Kirilin <106656570+dimayych02@users.noreply.github.com>
2025-05-19 00:36:38 +0400

    Create 128.go

---
+++ 128.go
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/longest-consecutive-sequence/description/
+// Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+// You must write an algorithm that runs in O(n) time.
+
+func longestConsecutive(nums []int) int {
+	hashMap := make(map[int]struct{}, len(nums))
+    var longest int
+	for _, val := range nums {
+		if _, ok := hashMap[val]; !ok {
+			hashMap[val] = struct{}{}
+		}
+	}
+    for _, val := range nums {
+        if _, ok := hashMap[val-1]; !ok {
+            length := 0
+            for; hashMap[val+length] == struct{}{}; length++ {}
+            longest = int(math.Max(float64(longest), float64(length)))
+        }
+    }
+    return longest
+}
