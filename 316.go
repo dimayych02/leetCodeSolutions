e39ea0b8ee9f188d9bcc0d7a053f7e1b64849877
Dmitrij Kirilin <106656570+dimayych02@users.noreply.github.com>
2025-05-22 00:49:00 +0400

    Update 316.go

--- 316.go
+++ 316.go
@@ -1,3 +1,7 @@
+// https://leetcode.com/problems/remove-duplicate-letters/description/
+// Given a string s, remove duplicate letters so that every letter appears once and only once.
+// You must make sure your result is the smallest in lexicographical order among all possible results.
+
 func removeDuplicateLetters(s string) string {
 	stack := make([]rune, 0, len(s))
 	hashMap := make(map[rune]int, len(s))
