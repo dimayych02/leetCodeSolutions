0ab7fa844921da2d5f6a5b1891da4676a467cdef
Dmitrij Kirilin <106656570+dimayych02@users.noreply.github.com>
2025-05-31 21:21:25 +0400

    Update 367.js

--- 367.js
+++ 367.js
@@ -2,16 +2,20 @@
 // A perfect square is an integer that is the square of an integer. In other words, it is the product of some integer with itself.
 // You must not use any built-in library function, such as sqrt. 
 
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
 var isPerfectSquare = function(num) {
     if (num == 1) {
         return true
     }
     
     let left = 2
-    let right = Number(num / 2).toFixed() + 1
+    let right = Math.floor(num / 2)
 
     while (left <= right) {
-        let midNum = Number((left + right) / 2).toFixed()
+        let midNum = Math.floor((left + right) / 2)
         let numsSquare = midNum*midNum
 
         if (numsSquare == num) {
@@ -27,3 +31,6 @@ var isPerfectSquare = function(num) {
     }
     return false
 };
+
+// 25 : [2,3,4,5,6,7,8,9,10,11,12]
+//
